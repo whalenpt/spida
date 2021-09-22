53b7902fa0cb3c3bbbdd195b5af55042045bf850
Patrick Whalen <whalenpt@gmail.com>
2021-09-22 10:39:30 -0700

    Moved helper constants to constexpr from constants

--- include/spida/helper/constants.h
+++ include/spida/helper/constants.h
@@ -1,19 +1,17 @@
-#ifndef SPIDA_CONSTANTS_H
-#define SPIDA_CONSTANTS_H
+//constants.h
+#pragma once
 
 #include <complex>
 #include <limits>
 #include <map>
 
 namespace spida{
-    const double PI = 3.141592653589793238462643383279502884197;
+    constexpr auto PI = 3.141592653589793238462643383279502884197;
 	using dcmplx = std::complex<double>;
-    const dcmplx ii (0.0,1.0);
-    const double NEAR_ZERO = 1.0e2*std::numeric_limits<double>::min();
+    constexpr dcmplx ii (0.0,1.0);
+    constexpr auto NEAR_ZERO = 1.0e2*std::numeric_limits<double>::min();
 	using stringMap = std::map<std::string,std::string>;
 	using stringPair = std::pair<std::string,std::string>;
 }
 
 
-
-#endif
