e13a6c62a64eb0bc8a470c6936c322721e6582b8
Patrick Whalen <whalenpt@gmail.com>
2026-05-02 11:59:00 -0700

    Restrict to static builds

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,5 @@
 rm -rf build && mkdir build && cd build
 cmake -S .. -DSPIDA_TEST=1 \
             -DSPIDA_DEMOS=1 \
-            -DSPIDA_STATIC=1 \
             -DCMAKE_EXPORT_COMPILE_COMMANDS=1
 cmake --build . --parallel 4
